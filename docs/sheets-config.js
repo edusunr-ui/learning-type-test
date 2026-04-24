2ab5573ad8bb4e6d0b032f9be80e5c5c28728f74
edusunr-ui <edusunr@gmail.com>
2026-04-24 19:21:48 +0900

    Connect Google Sheets endpoint

---
+++ docs/sheets-config.js
@@ -0,0 +1,5 @@
+window.LEARNING_TYPE_CONFIG = {
+  googleSheets: {
+    endpoint: "https://script.google.com/macros/s/AKfycbxiSz4z73GsCHnqO1KH0LgzdGY5cMYrx8mDEbwZBXe-mjOFQWeYz1s2oK8Um6obs85K/exec",
+  },
+};
